84aed0718a8cb5d56d3613cfcc678df0268c6710
Khalfi <128654961+noisebee20@users.noreply.github.com>
2025-06-23 20:40:27 +0100

    Initial commit

---
+++ lib/bloc/counter.bloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+
+abstract class CounterEvent{}
+ class IncrementCounterEvent extends CounterEvent{}
+
+ class DecrementCounterEvent extends CounterEvent{}
+
+class CounterState{
+  final int counter;
+  CounterState(this.counter);
+}
+class InitialState extends CounterState{
+  InitialState():super(0);
+}
+
+
+class CounterBloc extends Bloc<CounterEvent, CounterState>{
+  CounterBloc():super(InitialState()){
+    on((IncrementCounterEvent event, emit){
+      emit(CounterState(state.counter +1 ));
+    });
+    on((DecrementCounterEvent event, emit){
+      emit(CounterState(state.counter - 1));
+    });
+  }
+
+}
